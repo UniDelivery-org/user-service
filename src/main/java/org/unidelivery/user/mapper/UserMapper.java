b81748e578a7b2236b3c03ef336086209785d512
Achraf Sikal <achrafsikal@gmail.com>
2026-03-05 16:29:52 +0000

    feat(user): add updateEntityFromDto method to UserMapper for profile updates

--- src/main/java/org/unidelivery/user/mapper/UserMapper.java
+++ src/main/java/org/unidelivery/user/mapper/UserMapper.java
@@ -1,12 +1,18 @@
 package org.unidelivery.user.mapper;
 
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.unidelivery.user.dto.ProfileResponse;
 import org.unidelivery.user.dto.RegisterRequest;
+import org.unidelivery.user.dto.UpdateProfileRequestDTO;
 import org.unidelivery.user.model.User;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
     User toUser(RegisterRequest registerRequest);
     ProfileResponse toProfileResponse(User user);
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateEntityFromDto(UpdateProfileRequestDTO dto, @MappingTarget User entity);
 }
