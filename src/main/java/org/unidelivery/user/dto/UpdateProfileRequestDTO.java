291eee25ab72c55e7920faa7aaaf0bfe7c954f09
Achraf Sikal <achrafsikal@gmail.com>
2026-03-09 13:49:33 +0000

    feat(user): enhance UpdateProfileRequestDTO to support avatar uploads and deletion

--- src/main/java/org/unidelivery/user/dto/UpdateProfileRequestDTO.java
+++ src/main/java/org/unidelivery/user/dto/UpdateProfileRequestDTO.java
@@ -2,6 +2,7 @@ package org.unidelivery.user.dto;
 
 import jakarta.validation.constraints.*;
 import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
 
 @Data
 public class UpdateProfileRequestDTO {
@@ -16,12 +17,8 @@ public class UpdateProfileRequestDTO {
     @Pattern(regexp = "^\\+?[1-9]\\d{1,14}$", message = "Invalid phone number format")
     private String phone;
 
-    @Size(max = 500, message = "Avatar URL must not exceed 500 characters")
-    @Pattern(
-            regexp = "^(http?://.*)?$",
-            message = "Avatar URL must be a valid URL"
-    )
-    private String avatarUrl;
+    private MultipartFile avatar;
+    private boolean deleteAvatar;
 
     @DecimalMin(value = "-90.0", message = "Latitude must be >= -90")
     @DecimalMax(value = "90.0", message = "Latitude must be <= 90")
